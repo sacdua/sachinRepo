96e55c5c6f325df06848c5bc4e48772a93d40e3e
sacdu <sacdu@DESKTOP-FC1JV23>
2022-03-06 21:13:39 +0530

    added comment in student file...

--- LearnJavaAutomation/src/Assignment1/Student.java
+++ LearnJavaAutomation/src/Assignment1/Student.java
@@ -1,27 +1,25 @@
 package Assignment1;
 
-public class Student 
-{
+public class Student {
 	int age;
 	int roll_no;
-	public void display1()
-	{
+
+	public void display1() {
 		System.out.println("Welcome to all of you");
 	}
-	public void display2()
-	{
+
+	public void display2() {
 		System.out.println("Automation is very easy");
 	}
-	
-	public static void main(String[] args) 
-	{
+
+	public static void main(String[] args) {
 		Student obj = new Student();
-		obj.age=28;
-		obj.roll_no=123;
+		obj.age = 28;
+		obj.roll_no = 123;
 		System.out.println("Age is " + obj.age);
 		System.out.println("Roll no is " + obj.roll_no);
 		obj.display1();
-		obj.display2();		
+		obj.display2();
 	}
-
+//test comment
 }
